d807996818e4571168601510f4c3ea0e5c09cc23
RenishPatel537 <157466954+RenishPatel537@users.noreply.github.com>
2026-01-12 19:49:20 +0530

    fixed cors

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Cardio ML Backend")
 origins = [
     "http://localhost:3000",   # React / Next.js
     "http://127.0.0.1:3000",
-    "https://github.com/RenishPatel537/Cardio_ML_Project_backend"
+    "https://cardio-ml-project-frontend-8r11v906g-renish-andanis-projects.vercel.app",
 ]
 
 app.add_middleware(
